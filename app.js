f2cd2bcaeba6764ae26c4e9a9ad4f7469e9e7869
Lass <lass@MacBook-Pro-macbook.local>
2016-10-10 11:46:46 +0300

    add comments

--- app.js
+++ app.js
@@ -3,31 +3,30 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('currentWeather', function currentWeather($http, $scope) {
-    var key = 'dddcd2cbd1d2d744671a9cf971bcf18f';
+    var key = 'dddcd2cbd1d2d744671a9cf971bcf18f'; //Ключ для доступа к серверу
 
-    $scope.requestWeather = function(cityid) {
+    $scope.requestWeather = function(cityid) { //Запрос к выбраному городу по ID
 
         $http.get('http://api.openweathermap.org/data/2.5/weather?id='+cityid+'&appid='+key+'&units=metric').then(function (response) {
-            $scope.weather = response.data;
-            $scope.search = '';
-            $scope.resault = 'active';
-            console.log($scope.weather)
+            $scope.weather = response.data; //Полученые данные
+            $scope.search = ''; // Скрываем модальное окно с результатами поиска
+            $scope.resault = 'active'; // Показываем результат полученных данных
         })
     };
 
-    $scope.queryCity = function() {
-        var query = '';
-        if($scope.cityForm.$valid) {
-            query = $scope.city;
-            $scope.loader = 'active';
+    $scope.queryCity = function() { // Функция запроса подходящих городов по названию
+        var query = ''; // Очищаем данные запроса
+        if($scope.cityForm.$valid) { //Проверяем валидность введенных данных (минимум 3 символа)
+            query = $scope.city; //Записываем название города в объект $scope
+            $scope.loader = 'active'; //Показываем иконку поиска
             $http.get('http://api.openweathermap.org/data/2.5/find?q='+query+'&type=like&mode=json&appid=dddcd2cbd1d2d744671a9cf971bcf18f').then(function (response) {
-                var res = response.data,
-                    city_list = res["list"];
-                $scope.cities = city_list;
-                $scope.search = 'active';
-                $scope.loader = '';
+                var res = response.data, //Полученные данные
+                    city_list = res["list"]; //Получаем названия городов из полученного объекта
+                $scope.cities = city_list; //Записываем список городов в объект $scope
+                $scope.search = 'active'; //Показываем результаты поиска
+                $scope.loader = ''; // Скрываем иконку поиска
             })
-        } else {
+        } else { //Убираем результаты поиска и иконку поиска, очищаем данные для запроса, если они не валидны (менее 3 символов)
             query = '';
             $scope.loader = '';
             $scope.cities = '';
